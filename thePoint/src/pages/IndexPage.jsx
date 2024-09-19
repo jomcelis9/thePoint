22c20290c9d9b31c270981d6508cafbc9badb14c
jomcelis <jqcelis@addu.edu.ph>
2024-09-19 18:08:05 +0800

    FROM JOM'S BRANCH

--- thePoint/src/pages/IndexPage.jsx
+++ thePoint/src/pages/IndexPage.jsx
@@ -3,11 +3,11 @@ import Header from "../Header.jsx"
 
 export default function IndexPage(){
     return(
-        <div className="px-4">Index page here FROM JOM'S BRANCH
+        <div className="px-4">Index page here NEW
             <div className="flex justify-center">
             <Link to={'/booking'}>
                 <button type="button" className="bg-gradient-to-r from-thePointRed to-thePointPink text-white bg-primary-600 focus:ring-2 focus:outline-none focus:ring-amber-200 font-medium rounded-3xl text-sm px-5 py-2.5 text-center 
-                transform active:scale-x-100 transition-transform transition ease-in delay-100 hover:-translate-y-1 hover:drop-shadow-xl duration-300 "> JOM'S BRANCH</button>
+                transform active:scale-x-100 transition-transform transition ease-in delay-100 hover:-translate-y-1 hover:drop-shadow-xl duration-300 "> Book an Appointment Here pls pls pls from jmars branch wtf wtf </button>
             </Link>
             </div>
         </div>
