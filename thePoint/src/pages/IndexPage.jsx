2bd739f11d013823794351d94ca75f902691d81a
jomcelis <jqcelis@addu.edu.ph>
2024-09-19 18:05:46 +0800

    FROM JOM'S BRANCH TO MASTER

--- thePoint/src/pages/IndexPage.jsx
+++ thePoint/src/pages/IndexPage.jsx
@@ -7,7 +7,7 @@ export default function IndexPage(){
             <div className="flex justify-center">
             <Link to={'/booking'}>
                 <button type="button" className="bg-gradient-to-r from-thePointRed to-thePointPink text-white bg-primary-600 focus:ring-2 focus:outline-none focus:ring-amber-200 font-medium rounded-3xl text-sm px-5 py-2.5 text-center 
-                transform active:scale-x-100 transition-transform transition ease-in delay-100 hover:-translate-y-1 hover:drop-shadow-xl duration-300 "> NO APPOINTMENT</button>
+                transform active:scale-x-100 transition-transform transition ease-in delay-100 hover:-translate-y-1 hover:drop-shadow-xl duration-300 "> JOM'S BRANCH</button>
             </Link>
             </div>
         </div>
