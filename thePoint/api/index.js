48d073f6cc6a0f0c20559e062e2292c3649f2b58
jomcelis <jqcelis@addu.edu.ph>
2024-09-22 00:04:14 +0800

    changed baseURL

--- thePoint/api/index.js
+++ thePoint/api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use(express.json());
+
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5174',
@@ -9,6 +11,11 @@ app.use(cors({
 
 app.get('/test' , (req,res) => {
     res.json('test ok');
+});
+
+app.post('/register',(req,res) => {
+    const {name, email, password} = req.body;
+    res.json({name, email, password});
 });
 
 app.listen(4000);
