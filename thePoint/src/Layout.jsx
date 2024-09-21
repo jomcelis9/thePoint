41efed5f9363b51993dc3b7b66d03217807d2b9c
jomcelis <jqcelis@addu.edu.ph>
2024-09-21 17:32:14 +0800

    Adjust colors

--- thePoint/src/Layout.jsx
+++ thePoint/src/Layout.jsx
@@ -4,8 +4,10 @@ import Header from "./Header";
 export default function Layout(){
     return(
         <div className="">
-            <Header />
-            <Outlet />
+            <header class="sticky top-0 z-50">
+                <Header />
+            </header>
+                <Outlet />
         </div>
     )
 }
