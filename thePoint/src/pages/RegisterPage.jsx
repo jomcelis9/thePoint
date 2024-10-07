6f93749032444d38a4720ea8d0cc3f2f2dc43f0e
jomcelis <jqcelis@addu.edu.ph>
2024-10-07 15:51:48 +0800

    initial save

--- thePoint/src/pages/RegisterPage.jsx
+++ thePoint/src/pages/RegisterPage.jsx
@@ -21,26 +21,26 @@ function registerUser(ev){
 
     return(
         <div>
-      <div class="min-h-screen flex flex-col items-center justify-center">
-        <div class="grid md:grid-cols-2 items-center gap-4 max-md:gap-8 max-w-6xl max-md:max-w-lg w-full p-4 m-4 shadow-thePointRed60 shadow-2xl rounded-md">
-          <div class="md:max-w-md w-full px-4 py-4">
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="grid md:grid-cols-2 items-center gap-4 max-md:gap-8 max-w-6xl max-md:max-w-lg w-full p-4 m-4 shadow-thePointRed60 shadow-2xl rounded-md">
+          <div className="md:max-w-md w-full px-4 py-4">
             <div className="flex flex-col items-center justify-center">
                         <img src="/src/images/THE POINT LOGO tp 2.png" className="w-44 bg-transparent drop-shadow-2xl " alt="" />
             </div>
                 <form onSubmit={registerUser}>
-              <div class="mb-12">
-                <h3 class="text-gray-800 text-3xl font-bold">Register your account</h3>
+              <div className="mb-12">
+                <h3 className="text-gray-800 text-3xl font-bold">Register your account</h3>
               </div>
 
               <div className="grid grid-cols-2 gap-5 my-4">
 
               <div>
-                <label class="text-gray-800 text-xs block mb-2">Name</label>
-                <div class="relative flex items-center">
+                <label className="text-gray-800 text-xs block mb-2">Name</label>
+                <div className="relative flex items-center">
                   <input 
                   value={name} onChange={ev => setName(ev.target.value)}
-                  name="registerFirstName" type="text"  class="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="First name" />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
+                  name="registerFirstName" type="text"  className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="First name" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                     <defs>
                       <clipPath id="a" clipPathUnits="userSpaceOnUse">
                         <path d="M0 512h512V0H0Z" data-original="#000000"></path>
@@ -56,12 +56,12 @@ function registerUser(ev){
 
               
               <div>
-                <label class="text-gray-800 text-xs block mb-2">Last Name</label>
-                <div class="relative flex items-center">
+                <label className="text-gray-800 text-xs block mb-2">Last Name</label>
+                <div className="relative flex items-center">
                   <input 
                   value={lastName} onChange={ev => setLastName(ev.target.value)}
-                  name="registerLastName" type="text"  class="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="Last name" />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
+                  name="registerLastName" type="text"  className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="Last name" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                     <defs>
                       <clipPath id="a" clipPathUnits="userSpaceOnUse">
                         <path d="M0 512h512V0H0Z" data-original="#000000"></path>
@@ -79,12 +79,12 @@ function registerUser(ev){
               </div>
               
               <div>
-                <label class="text-gray-800 text-xs block mb-2">Email</label>
-                <div class="relative flex items-center">
+                <label className="text-gray-800 text-xs block mb-2">Email</label>
+                <div className="relative flex items-center">
                   <input 
                   value={email} onChange={ev => setEmail(ev.target.value)}
-                  name="registerEmail" type="text"  class="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="Enter email" />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
+                  name="registerEmail" type="text"  className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="Enter email" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                     <defs>
                       <clipPath id="a" clipPathUnits="userSpaceOnUse">
                         <path d="M0 512h512V0H0Z" data-original="#000000"></path>
@@ -98,13 +98,13 @@ function registerUser(ev){
                 </div>
               </div>
 
-              <div class="mt-4">
-                <label class="text-gray-800 text-xs block mb-2">Password</label>
-                <div class="relative flex items-center">
+              <div className="mt-4">
+                <label className="text-gray-800 text-xs block mb-2">Password</label>
+                <div className="relative flex items-center">
                   <input 
                   value={password} onChange={ev => setPassword(ev.target.value)}
-                  name="registerPassword" type="password"  class="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="Enter password" />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
+                  name="registerPassword" type="password"  className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-thePointPink px-2 py-3 outline-none" placeholder="Enter password" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
                     <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                   </svg>
                 </div>
