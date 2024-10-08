c40c1d301fbf9c1fd53e5bb19252dfd4ca4a7fff
jomcelis <jqcelis@addu.edu.ph>
2024-10-08 15:44:52 +0800

    table component

---
+++ thePoint/components/table.jsx
@@ -0,0 +1,25 @@
+export default function Table(){
+return(
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="p-4">
+                    <div className="flex items-center">
+                        <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                        <label for="checkbox-all-search" className="sr-only">checkbox</label>
+                    </div>
+                </th>
+            </tr>
+
+        </thead>
+
+    </table>
+
+    <h1> TABLe</h1>
+
+
+    </div>
+)
+
+}
--- thePoint/src/pages/admin/ViewAppointments.jsx
+++ thePoint/src/pages/admin/ViewAppointments.jsx
@@ -1,8 +1,16 @@
-export default function ViewAppointments(){
-    return(
-        <div className="relative">View appointments page
+import { useOutletContext } from "react-router-dom";
+import Table from "../../../components/table";
 
-        </div>
-        
-    );
-};
+export default function ViewAppointments() {
+  // Get the 'open' state from the AdminLayout component
+  const { open } = useOutletContext();
+
+  return (
+    <div className={`p-4 transition-all duration-300 ${open ? "ml-0" : "ml-20"} mt-20 border`}>
+      <h1 className="text-xl"> Pending appointments</h1>
+      {/* Table  */}
+            <Table/>
+
+    </div>
+  );
+}
