d8c2d175aa1da28e6d72dcf291d68b0e05b92def
jomcelis <jqcelis@addu.edu.ph>
2024-10-08 18:18:46 +0800

    updated table appointments

--- frontend/thePoint/components/table.jsx
+++ frontend/thePoint/components/table.jsx
@@ -35,6 +35,46 @@ return(
 
         </thead>
 
+        <tbody>
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 cursor-pointer ">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Juan
+                </th>
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Dela Cruz
+                </th>
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    09468138079
+                </th>
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    1234567890
+                </th>
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    09/11/2024
+                </th>
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    69:00
+                </th>
+                <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white gap-2 ">
+                    <div className="flex justify-center items-center" >
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                            <path d="M 41.9375 8.625 C 41.273438 8.648438 40.664063 9 40.3125 9.5625 L 21.5 38.34375 L 9.3125 27.8125 C 8.789063 27.269531 8.003906 27.066406 7.28125 27.292969 C 6.5625 27.515625 6.027344 28.125 5.902344 28.867188 C 5.777344 29.613281 6.078125 30.363281 6.6875 30.8125 L 20.625 42.875 C 21.0625 43.246094 21.640625 43.410156 22.207031 43.328125 C 22.777344 43.242188 23.28125 42.917969 23.59375 42.4375 L 43.6875 11.75 C 44.117188 11.121094 44.152344 10.308594 43.78125 9.644531 C 43.410156 8.984375 42.695313 8.589844 41.9375 8.625 Z"></path>
+                        </svg>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                            <path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
+                        </svg>
+                    </div>
+                </th>
+            </tr>
+        </tbody>
+
     </table>
 
     <h1> TABLe</h1>
