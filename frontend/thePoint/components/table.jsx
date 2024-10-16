ad09c955b5648cf5d420fbc3c9c77ee642a6eae4
jomcelis <jqcelis@addu.edu.ph>
2024-10-16 14:58:30 +0800

    Addedd 'appointmentNumber' as key

--- frontend/thePoint/components/table.jsx
+++ frontend/thePoint/components/table.jsx
@@ -71,7 +71,11 @@ return(
         <tbody>
             
           {/* Map through the data and generate rows */}
-          {data.map((row, index) => (
+          {data.map((row, index) => {
+
+            const appointmentNumber = Object.values(row)[3];
+
+          return (
             <tr key={index}>
                 <tr>
                     <th scope="col" className="p-4">
@@ -81,11 +85,44 @@ return(
                         </div>
                     </th>
                 </tr>
-              {Object.values(row).map((val, i) => (
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" key={i}>{val}</th>
-              ))}
+
+                {Object.values(row).slice(0, -1).map((val,i) => (
+
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" key={appointmentNumber}>
+                    {val}             
+                </th>
+
+                ))}
+
+              <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white gap-2 ">
+                        <div className="flex justify-center items-center" >
+                            <button
+                            type="button"
+                            className="transform active:scale-x-100 transition-transform transition ease-in-out delay-150 hover:-translate-y-1 duration-300 
+                            shadow-md bg-gradient-to-r from-thePointRed to-thePointPink text-white font-bold rounded-full text-sm px-2 py-1 
+                            text-center mr-7">
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" transform="rotate(0 0 0)">
+                                <path d="M19.2803 6.76264C19.5732 7.05553 19.5732 7.53041 19.2803 7.8233L9.86348 17.2402C9.57058 17.533 9.09571 17.533 8.80282 17.2402L4.71967 13.157C4.42678 12.8641 4.42678 12.3892 4.71967 12.0963C5.01256 11.8035 5.48744 11.8035 5.78033 12.0963L9.33315 15.6492L18.2197 6.76264C18.5126 6.46975 18.9874 6.46975 19.2803 6.76264Z" fill="#ffffff"/>
+                                </svg>
+                            </button>  
+    
+                            <button
+                            type="button"
+                            className="transform active:scale-x-100 transition-transform transition ease-in-out delay-150 hover:-translate-y-1 duration-300 
+                            bg-transparent text-white font-bold rounded-full text-sm
+                            text-center">
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="#343C54" xmlns="http://www.w3.org/2000/svg" transform="rotate(0 0 0)">
+                                <path d="M6.21967 7.28033C5.92678 6.98744 5.92678 6.51256 6.21967 6.21967C6.51256 5.92678 6.98744 5.92678 7.28033 6.21967L11.999 10.9384L16.7176 6.2198C17.0105 5.92691 17.4854 5.92691 17.7782 6.2198C18.0711 6.51269 18.0711 6.98757 17.7782 7.28046L13.0597 11.999L17.7782 16.7176C18.0711 17.0105 18.0711 17.4854 17.7782 17.7782C17.4854 18.0711 17.0105 18.0711 16.7176 17.7782L11.999 13.0597L7.28033 17.7784C6.98744 18.0713 6.51256 18.0713 6.21967 17.7784C5.92678 17.4855 5.92678 17.0106 6.21967 16.7177L10.9384 11.999L6.21967 7.28033Z" fill="#343C54"/>
+                                </svg>
+                            </button> 
+    
+                        </div>
+                    </th>
             </tr>
-          ))}
+          );
+          })}
+
+        
 
         </tbody>
 
