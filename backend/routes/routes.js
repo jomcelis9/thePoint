5aed7dcdf0c1a986eeb2defe666132296c35ee4c
jomcelis <jqcelis@addu.edu.ph>
2024-10-25 02:17:38 +0800

    db connection for jom

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,8 +6,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'thePoint',
-    password: 'admin123',
+    database: 'postgres',
+    password: '123',
     port: 5432,
 
 });
