de545e80d5f04b1536a03aa48c232bfa2433c41b
jomcelis <jqcelis@addu.edu.ph>
2024-11-07 15:13:28 +0800

    Db Changes

--- backend/routes/routes 2.js
+++ backend/routes/routes 2.js
@@ -6,8 +6,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'postgres2',
-    password: 'geraldcool12',
+    database: 'postgres',
+    password: '123',
     port: 5432,
 
 });
